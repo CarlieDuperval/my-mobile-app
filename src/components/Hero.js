518291510d2834f3ced603232e389c3baf617ad8
Carlie Duperval <carlierichell@gmail.com>
2022-05-24 16:42:33 -0400

    Grab  picture from the web

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,6 +5,7 @@ export default function Hero () {
         // All image need a size , It is really important
         <Image  
         style={{ width: 406, height: 232}}
+        // adding picture grab from the web
         source={{ 
             uri:"https://www.bocacode.com/assets/images/2021.7.27-BocaCode-Web-53.png"
     }} />
